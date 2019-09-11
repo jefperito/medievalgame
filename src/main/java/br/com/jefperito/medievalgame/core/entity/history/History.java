48cfd48f41df1158296472067f08d865ed2adfc5
Jeferson Viana Perito <jefperito@gmail.com>
2019-09-10 22:07:39 -0300

    Removing unecessary code

--- src/main/java/br/com/jefperito/medievalgame/core/entity/history/History.java
+++ src/main/java/br/com/jefperito/medievalgame/core/entity/history/History.java
@@ -3,19 +3,6 @@ package br.com.jefperito.medievalgame.core.entity.history;
 import br.com.jefperito.medievalgame.core.entity.action.CharacterAction;
 import br.com.jefperito.medievalgame.core.entity.consequence.Consequence;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class History {
-    private List<HistoryLine> historyLines;
-
-    public History() {
-        historyLines = new ArrayList<>();
-    }
-
-    public void addHistoryLine(HistoryLine historyLine) {
-        historyLines.add(historyLine);
-    }
-
     public abstract Consequence createConsequence(CharacterAction characterAction);
 }
